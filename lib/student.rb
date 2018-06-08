facd8b42934640d0dc06fc101769ccd36267e378
Carrie Savinell <carrielane89@gmail.com>
2018-06-08 21:27:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each {|key, value| send("#{key}=", value)}
+    student_hash.each {|key, value| send("#{key}=", value)
+     binding.pry}
      @@all << self
   end
 
